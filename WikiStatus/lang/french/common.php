610ec6f13827380455a2ab5bc29d9857c34e345c
mipel <mipel@phpboost.com>
2017-02-01 20:01:37 +0100

    [MAJ]ajout du module WikiStatus

---
+++ WikiStatus/lang/french/common.php
@@ -0,0 +1,41 @@
+<?php
+/*##################################################
+ *                               WikiStatusModuleMiniMenu.class.php
+ *                            -------------------
+ *   begin                : January 30, 2017
+ *   copyright            : (C) 2016 Julien BRISWALTER
+ *   email                : j1.seth@phpboost.com
+ *
+ *
+ ###################################################
+ *
+ * This program is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ ###################################################*/
+
+
+ ####################################################
+#                                                          French                                                                        #
+ ####################################################
+
+$lang['menu_title'] = 'Articles WIKI créés ou mis à jour';
+$lang['title_date_and_time'] = 'Date et heure';
+$lang['title_author'] = 'Auteur';
+$lang['statut_1'] = 'Article OK';
+$lang['statut_2'] = 'Incomplet';
+$lang['statut_3'] = 'En travaux';
+$lang['statut_4'] = 'A refaire';
+$lang['statut_5'] = 'Obsolète';
+?>
---
+++ WikiStatus/templates/ArticlesWikiStatusUpdated.tpl
@@ -0,0 +1,28 @@
+<span class="span-mini-wiki-status">
+Seuls les documents dont le statut a été déclaré (prédéfini ou personnalisé) sont affichés.
+</span>
+<table id="table-mini-wiki-status">
+	<thead>
+	<tr>
+		<th width=25%>{@title_date_and_time}</th>
+		<th>Article</th>
+		<th>Statut</th>
+		<th width=15%>{@title_author}</th>
+	</tr>
+	</thead>
+	<tbody>
+		# START articles_wiki_items #
+		<tr>
+			<td height=75px>{articles_wiki_items.DATE}</td>
+			<td><a href="{articles_wiki_items.U_ARTICLE}" title="{articles_wiki_items.TITLE}">{articles_wiki_items.TITLE}</a></td>
+			<td><span class="{articles_wiki_items.STATUS_CLASS}">{articles_wiki_items.STATUS}</span></td>
+			<td># IF articles_wiki_items.C_AUTHOR_EXIST #<a href="{articles_wiki_items.U_AUTHOR_PROFILE}" class="{articles_wiki_items.USER_LEVEL_CLASS}" # IF articles_wiki_items.C_USER_GROUP_COLOR # style="color:{articles_wiki_items.USER_GROUP_COLOR}"# ENDIF #>{articles_wiki_items.PSEUDO}</a># ELSE #{articles_wiki_items.AUTHOR_IP}# ENDIF #</td>
+		</tr>
+		# END articles_wiki_items #
+	</tbody>
+</table>
+
+<script>
+	jQuery('#table-mini-wiki-status').basictable();
+</script>
+ 
