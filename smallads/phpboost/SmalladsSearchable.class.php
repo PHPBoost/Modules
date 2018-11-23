7d914fa2b29f340acc3adb8edaf258e8b88c9a2d
j1.seth <julien.briswalter@gmail.com>
2018-11-23 15:56:44 +0100

    [MAJ] #1844 [recherche] ajout mots clés

--- smallads/phpboost/SmalladsSearchable.class.php
+++ smallads/phpboost/SmalladsSearchable.class.php
@@ -45,7 +45,9 @@ class SmalladsSearchable extends AbstractSearchableExtensionPoint
 			CONCAT('" . PATH_TO_ROOT . "/smallads/" . (!ServerEnvironmentConfig::load()->is_url_rewriting_enabled() ? "index.php?url=/" : "") . "', id_category, '-', IF(id_category != 0, cat.rewrited_name, 'root'), '/', smallads.id, '-', smallads.rewrited_title) AS link
 			FROM " . SmalladsSetup::$smallads_table . " smallads
 			LEFT JOIN ". SmalladsSetup::$smallads_cats_table ." cat ON cat.id = smallads.id_category
-			WHERE ( FT_SEARCH(smallads.title, '" . $args['search'] . "') OR FT_SEARCH(smallads.contents, '" . $args['search'] . "') OR FT_SEARCH_RELEVANCE(smallads.description, '" . $args['search'] . "') )
+			LEFT JOIN ". DB_TABLE_KEYWORDS_RELATIONS ." relation ON relation.module_id = 'smallads' AND relation.id_in_module = smallads.id
+			LEFT JOIN ". DB_TABLE_KEYWORDS ." keyword ON keyword.id = relation.id_keyword
+			WHERE ( FT_SEARCH(smallads.title, '" . $args['search'] . "') OR FT_SEARCH(smallads.contents, '" . $args['search'] . "') OR FT_SEARCH_RELEVANCE(smallads.description, '" . $args['search'] . "') ) OR keyword.rewrited_name = '" . Url::encode_rewrite($args['search']) . "'
 			AND id_category IN(" . implode(", ", $authorized_categories) . ")
 			AND (published = 1 OR (published = 2 AND publication_start_date < '" . $now->get_timestamp() . "' AND (publication_end_date > '" . $now->get_timestamp() . "' OR publication_end_date = 0)))
 			ORDER BY relevance DESC
