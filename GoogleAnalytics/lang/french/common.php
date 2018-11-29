60206bc39730477584abcf65ee311240ea78c330
mipel <mipel@phpboost.com>
2018-11-29 12:28:17 +0100

    [MAJ] fautes orthographe dans lang GoogleAnalytics

--- GoogleAnalytics/lang/french/common.php
+++ GoogleAnalytics/lang/french/common.php
@@ -34,7 +34,7 @@ $lang['name'] = 'Google Analytics';
 
 $lang['configuration'] = 'Configuration';
 $lang['identifier'] = 'Identifiant de suivi du site';
-$lang['identifier.explain'] = 'Fourni dans l\'interface de Google Analytics. Rubrique Administration/Paramêtres du compte/Numéro de compte';
-$lang['identifier_required'] = 'L\'idenfiant de Google Analytics n\'est pas présent dans la configuration du module. <a href=":link">Veuillez le renseigner pour faire fonctionner le module correctement.</a>';
+$lang['identifier.explain'] = 'Fourni dans l\'interface de Google Analytics. Rubrique Administration/Paramètres du compte/Numéro de compte';
+$lang['identifier_required'] = 'L\'identifiant de Google Analytics n\'est pas présent dans la configuration du module. <a href=":link">Veuillez le renseigner pour faire fonctionner le module correctement.</a>';
 $lang['analytics_login'] = '<a href=":link">Connectez vous à votre compte Google Analytics pour voir l\'audience et les statistiques.</a>';
 ?>
