324b99484034f42cb72d4a9e28a035e812841d2e
Labsoweb\babsolune <babsolune@phpboost.com>
2019-01-08 10:15:29 +0100

    [MAJ] [Smallads] dockblock

--- smallads/index.php
+++ smallads/index.php
@@ -2,7 +2,7 @@
 /**
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
- * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @author      Julien BRISWALTER <j1.seth@phpboost.com>
  * @version   	PHPBoost 5.2 - last update: 2018 12 11
  * @since   	PHPBoost 4.0 - 2013 01 30
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
