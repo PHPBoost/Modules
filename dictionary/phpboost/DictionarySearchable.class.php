dde79b5d5a59ac1eec73aa7cac065891984d2e2e
j1.seth <julien.briswalter@gmail.com>
2019-08-20 22:27:47 +0200

    [MAJ] Recherche : utilisation de la nouvelle classe par défaut

--- dictionary/phpboost/DictionarySearchable.class.php
+++ dictionary/phpboost/DictionarySearchable.class.php
@@ -3,30 +3,25 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2016 02 17
+ * @version   	PHPBoost 5.3 - last update: 2019 08 20
  * @since   	PHPBoost 3.0 - 2012 11 15
 */
 
-class DictionarySearchable extends AbstractSearchableExtensionPoint
+class DictionarySearchable extends DefaultSearchable
 {
-	public function get_search_request($args)
+	public function __construct()
 	{
-		$weight = isset($args['weight']) && is_numeric($args['weight']) ? $args['weight'] : 1;
-
-		if (DictionaryAuthorizationsService::check_authorizations()->read())
-		{
-			return "SELECT " . $args['id_search'] . " AS id_search,
-			q.id AS id_content,
-			q.word AS title,
-			( 2 * FT_SEARCH_RELEVANCE(q.word, '" . $args['search'] . "') + FT_SEARCH_RELEVANCE(q.description, '" . $args['search'] . "') ) / 3 * " . $weight . " AS relevance,
-			CONCAT('" . PATH_TO_ROOT . "/dictionary/dictionary.php?l=', q.word) AS link
-			FROM " . PREFIX . "dictionary q
-			WHERE ( FT_SEARCH(q.word, '" . $args['search'] . "') OR FT_SEARCH(q.description, '" . $args['search'] . "') )
-			ORDER BY relevance DESC
-			LIMIT 100 OFFSET 0";
-		}
-
-		return '';
+		parent::__construct('dictionary');
+		$this->read_authorization = DictionaryAuthorizationsService::check_authorizations()->read();
+		
+		$this->table_name = PREFIX . 'dictionary';
+		
+		$this->custom_all_link = "'" . PATH_TO_ROOT . "/dictionary/dictionary.php?l=', word";
+		
+		$this->field_title = 'word';
+		$this->field_contents = 'description';
+		
+		$this->has_approbation = false;
 	}
 }
 ?>
--- smallads/phpboost/SmalladsSearchable.class.php
+++ smallads/phpboost/SmalladsSearchable.class.php
@@ -3,35 +3,33 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2019 02 13
+ * @version   	PHPBoost 5.3 - last update: 2019 08 20
  * @since   	PHPBoost 4.0 - 2013 01 29
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
 class SmalladsSearchable extends AbstractSearchableExtensionPoint
 {
-	public function get_search_request($args)
+	public function __construct()
 	{
-		$now = new Date();
-		$authorized_categories = SmalladsService::get_authorized_categories(Category::ROOT_CATEGORY);
-		$weight = isset($args['weight']) && is_numeric($args['weight']) ? $args['weight'] : 1;
-
-		return "SELECT " . $args['id_search'] . " AS id_search,
-			smallads.id AS id_content,
-			smallads.title AS title,
-			(2 * FT_SEARCH_RELEVANCE(smallads.title, '" . $args['search'] . "') + (FT_SEARCH_RELEVANCE(smallads.contents, '" . $args['search'] . "') +
-			FT_SEARCH_RELEVANCE(smallads.description, '" . $args['search'] . "')) / 2 ) / 3 * " . $weight . " AS relevance,
-			CONCAT('" . PATH_TO_ROOT . "/smallads/" . (!ServerEnvironmentConfig::load()->is_url_rewriting_enabled() ? "index.php?url=/" : "") . "', id_category, '-', IF(id_category != 0, cat.rewrited_name, 'root'), '/', smallads.id, '-', smallads.rewrited_title) AS link
-			FROM " . SmalladsSetup::$smallads_table . " smallads
-			LEFT JOIN ". SmalladsSetup::$smallads_cats_table ." cat ON cat.id = smallads.id_category
-			LEFT JOIN ". DB_TABLE_KEYWORDS_RELATIONS ." relation ON relation.module_id = 'smallads' AND relation.id_in_module = smallads.id
-			LEFT JOIN ". DB_TABLE_KEYWORDS ." keyword ON keyword.id = relation.id_keyword
-			WHERE ( FT_SEARCH(smallads.title, '" . $args['search'] . "') OR FT_SEARCH(smallads.contents, '" . $args['search'] . "') OR FT_SEARCH_RELEVANCE(smallads.description, '" . $args['search'] . "') ) OR keyword.rewrited_name = '" . Url::encode_rewrite($args['search']) . "'
-			AND id_category IN(" . implode(", ", $authorized_categories) . ")
-			AND (published = 1 OR (published = 2 AND publication_start_date < '" . $now->get_timestamp() . "' AND (publication_end_date > '" . $now->get_timestamp() . "' OR publication_end_date = 0)))
-			GROUP BY id_content
-			ORDER BY relevance DESC
-			LIMIT 100 OFFSET 0";
+		parent::__construct('smallads');
+		$this->read_authorization = SmalladsAuthorizationsService::check_authorizations()->read();
+		
+		$this->table_name = SmalladsSetup::$smallads_table;
+		
+		$this->cats_table_name = SmalladsSetup::$smallads_cats_table;
+		$this->authorized_categories = SmalladsService::get_authorized_categories(Category::ROOT_CATEGORY);
+		
+		$this->use_keywords = true;
+		
+		$this->has_short_contents = true;
+		$this->field_short_contents = 'description';
+		
+		$this->field_approbation_type = 'published';
+		
+		$this->has_validation_period = true;
+		$this->field_validation_start_date = 'publishing_start_date';
+		$this->field_validation_end_date = 'publishing_end_date';
 	}
 }
 ?>
