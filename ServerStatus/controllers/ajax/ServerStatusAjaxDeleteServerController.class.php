8d59d15ec06ba720517b3425508086ea3ed58656
Julien Briswalter <jbriswalter@users.noreply.github.com>
2019-10-17 08:48:22 +0200

    [FIX] ServerStatus delete server

--- ServerStatus/controllers/ajax/ServerStatusAjaxDeleteServerController.class.php
+++ ServerStatus/controllers/ajax/ServerStatusAjaxDeleteServerController.class.php
@@ -3,7 +3,7 @@
  * @copyright 	&copy; 2005-2019 PHPBoost
  * @license 	https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version   	PHPBoost 5.2 - last update: 2016 02 11
+ * @version   	PHPBoost 5.2 - last update: 2019 10 17
  * @since   	PHPBoost 4.0 - 2013 08 20
 */
 
