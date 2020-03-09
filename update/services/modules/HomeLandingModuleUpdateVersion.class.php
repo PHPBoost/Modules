268d1b53cfb146f9139bbb1efb7092a4b7a7e005
Labsoweb\babsolune <babsolune@phpboost.com>
2020-03-09 04:49:26 +0100

    [UPD] HomeLanding update

---
+++ update/services/modules/HomeLandingModuleUpdateVersion.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2020 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 5.3 - last update: 2020 03 09
+ * @since       PHPBoost 5.2 - 2020 03 09
+*/
+
+class HomeLandingModuleUpdateVersion extends ModuleUpdateVersion
+{
+	public function __construct()
+	{
+		parent::__construct('HomeLanding');
+
+		$this->content_tables = array(PREFIX . 'HomeLanding');
+		$this->delete_old_files_list = array(
+			'/templates/pagecontent/onepage.tpl'
+		);
+
+		// HomeLandingConfig::init_modules_array();
+	}
+}
+?>
