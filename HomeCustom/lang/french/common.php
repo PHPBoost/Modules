6e92603337ece2a1d9ef9364f41f4fd798a2aa24
mipel85 <mipel@phpboost.com>
2020-03-27 17:10:39 +0100

    [UPD] correction of spelling mistakes

--- HomeCustom/lang/french/common.php
+++ HomeCustom/lang/french/common.php
@@ -3,9 +3,10 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2019 10 09
+ * @version     PHPBoost 5.3 - last update: 2020 03 27
  * @since       PHPBoost 3.0 - 2012 08 28
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @contributor Mipel <mipel@phpboost.com>
 */
 
 ####################################################
@@ -13,5 +14,5 @@
 ####################################################
 
 $lang['homecustom.module.title'] = 'Votre page d\'accueil personnalisée';
-$lang['homecustom.documentation'] = 'Acceder au didacticiel d\'utilisation ce module, dans la documentation de PHPBoost';
+$lang['homecustom.documentation'] = 'Accéder au didacticiel d\'utilisation de ce module dans la documentation de PHPBoost';
 ?>
--- countdown/lang/french/common.php
+++ countdown/lang/french/common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2017 06 16
+ * @version     PHPBoost 5.3 - last update: 2020 03 27
  * @since       PHPBoost 4.1 - 2014 12 12
 */
 
@@ -29,7 +29,7 @@ $lang['config.authorizations.read']  = 'Autorisation d\'afficher le compte à re
 $lang['config.stop.counter'] = 'Arrêter le compteur à la fin du décompte';
 $lang['config.stop.counter.desc'] = 'Si non coché, le compteur continue';
 $lang['config.hidden.counter'] = 'Cacher le compteur à la fin du décompte';
-$lang['config.stopped.event'] = 'Événement Terminé';
+$lang['config.stopped.event'] = 'Événement terminé';
 $lang['config.stopped.event.desc'] = 'Message à afficher quand le décompte s\'arrête';
 
 // Gestion de l'affichage
