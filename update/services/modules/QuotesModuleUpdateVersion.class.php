fa813f5fbfca0403714a712f2badbd572a1694d0
j1.seth <julien.briswalter@gmail.com>
2020-05-02 23:58:55 +0200

    [UPD] Quotes update

--- update/services/modules/QuotesModuleUpdateVersion.class.php
+++ update/services/modules/QuotesModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 15
+ * @version     PHPBoost 5.3 - last update: 2020 05 02
  * @since       PHPBoost 5.3 - 2019 11 03
  * @contributor xela <xela@phpboost.com>
 */
@@ -14,7 +14,7 @@ class QuotesModuleUpdateVersion extends ModuleUpdateVersion
 	{
 		parent::__construct('quotes');
 
-		$this->content_tables = array(array('name' => PREFIX . 'quotes', 'contents' => 'quote'));
+		$this->content_tables = array(array('name' => PREFIX . 'quotes', 'content_field' => 'quote'));
 		$this->delete_old_files_list = array(
 			'/controllers/AdminQuotesManageController.class.php',
 			'/lang/english/config.php',
