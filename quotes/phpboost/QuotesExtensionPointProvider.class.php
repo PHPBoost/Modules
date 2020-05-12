08aca4679177a30d79a064328dcfd9108d06e535
j1.seth <julien.briswalter@gmail.com>
2020-05-12 08:19:45 +0200

    [UPD] Modules ExtensionPointProvider

--- quotes/phpboost/QuotesExtensionPointProvider.class.php
+++ quotes/phpboost/QuotesExtensionPointProvider.class.php
@@ -3,33 +3,18 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 15
+ * @version     PHPBoost 5.3 - last update: 2020 05 12
  * @since       PHPBoost 5.0 - 2016 02 18
  * @contributor mipel <mipel@phpboost.com>
  * @contributor xela <xela@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
-class QuotesExtensionPointProvider extends ModuleExtensionPointProvider
+class QuotesExtensionPointProvider extends ItemsModuleExtensionPointProvider
 {
-	public function __construct()
-	{
-		parent::__construct('quotes');
-	}
-
 	public function home_page()
 	{
 		return new DefaultHomePageDisplay($this->get_id(), QuotesDisplayCategoryController::get_view());
-	}
-
-	public function menus()
-	{
-		return new ModuleMenus(array(new QuotesModuleMiniMenu()));
-	}
-
-	public function search()
-	{
-		return new QuotesSearchable();
 	}
 }
 ?>
--- smallads/phpboost/SmalladsExtensionPointProvider.class.php
+++ smallads/phpboost/SmalladsExtensionPointProvider.class.php
@@ -3,42 +3,17 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 01 15
+ * @version     PHPBoost 5.3 - last update: 2020 05 12
  * @since       PHPBoost 4.0 - 2013 01 29
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
  * @contributor xela <xela@phpboost.com>
 */
 
-class SmalladsExtensionPointProvider extends ModuleExtensionPointProvider
+class SmalladsExtensionPointProvider extends ItemsModuleExtensionPointProvider
 {
-	public function __construct()
-	{
-		parent::__construct('smallads');
-	}
-
-	public function menus()
-	{
-		return new ModuleMenus(array(new SmalladsLastItemsMiniMenu()));
-	}
-
-	public function feeds()
-	{
-		return new SmalladsFeedProvider();
-	}
-
 	public function home_page()
 	{
 		return new DefaultHomePageDisplay($this->get_id(), SmalladsDisplayCategoryController::get_view());
-	}
-
-	public function scheduled_jobs()
-	{
-		return new SmalladsScheduledJobs();
-	}
-
-	public function search()
-	{
-		return new SmalladsSearchable();
 	}
 }
 ?>
