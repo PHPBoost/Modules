867d71446e2e76d18e368a3d514db8ac7f8bff76
j1.seth <julien.briswalter@gmail.com>
2020-05-14 07:33:36 +0200

    [FIX] Search

--- smallads/phpboost/SmalladsSearchable.class.php
+++ smallads/phpboost/SmalladsSearchable.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 5.3 - last update: 2020 02 06
+ * @version     PHPBoost 5.3 - last update: 2020 05 14
  * @since       PHPBoost 4.0 - 2013 01 29
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
@@ -18,6 +18,8 @@ class SmalladsSearchable extends DefaultSearchable
 		$this->table_name = SmalladsSetup::$smallads_table;
 		
 		$this->authorized_categories = CategoriesService::get_authorized_categories(Category::ROOT_CATEGORY, SmalladsConfig::load()->are_descriptions_displayed_to_guests(), $module_id);
+		
+		$this->field_content = 'contents';
 		
 		$this->use_keywords = true;
 		
