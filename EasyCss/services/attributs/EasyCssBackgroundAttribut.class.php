2cc0a876cdba24db0b759a7b6fed07e4cd33b0a3
Labsoweb\babsolune <babsolune@phpboost.com>
2020-12-15 05:48:28 +0100

    [FIX] php 8 EasyCss

--- EasyCss/services/attributs/EasyCssBackgroundAttribut.class.php
+++ EasyCss/services/attributs/EasyCssBackgroundAttribut.class.php
@@ -3,10 +3,11 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      PaperToss <t0ssp4p3r@gmail.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 14
+ * @version     PHPBoost 6.0 - last update: 2020 12 15
  * @since       PHPBoost 5.0 - 2016 06 03
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  * @contributor mipel <mipel@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 class EasyCssBackgroundAttribut extends EasyCssAbstractAttribut
@@ -45,10 +46,9 @@ class EasyCssBackgroundAttribut extends EasyCssAbstractAttribut
         }
     }
 
-    public function get_templates()
+    public function get_templates($label = '', $tpl = [])
     {
         AdminEasyCssEditController::add_field_to_hidden_input($this->parent_id . '/' . $this->id);
-        $tpls = [];
         foreach ($this->values as $key => $tpl)
         {
             $templates = $tpl->get_templates();
--- EasyCss/services/attributs/EasyCssBorderAttribut.class.php
+++ EasyCss/services/attributs/EasyCssBorderAttribut.class.php
@@ -3,10 +3,11 @@
  * @copyright   &copy; 2005-2020 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      PaperToss <t0ssp4p3r@gmail.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 14
+ * @version     PHPBoost 6.0 - last update: 2020 12 15
  * @since       PHPBoost 5.0 - 2016 04 22
  * @contributor mipel <mipel@phpboost.com>
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 class EasyCssBorderAttribut extends EasyCssAbstractAttribut
@@ -41,10 +42,9 @@ class EasyCssBorderAttribut extends EasyCssAbstractAttribut
         }
     }
 
-    public function get_templates()
+    public function get_templates($label = '', $tpl = [])
     {
         AdminEasyCssEditController::add_field_to_hidden_input($this->parent_id . '/' . $this->id);
-        $tpls = [];
         foreach ($this->values as $tpl)
         {
             $templates = $tpl->get_templates();
