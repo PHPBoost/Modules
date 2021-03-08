bc3797c51fc2f64fef3fc9a64350c1fb76c0b629
j1.seth <julien.briswalter@gmail.com>
2021-03-09 00:02:32 +0100

    [UPD] basic module french lang

---
+++ basic/lang/french/common.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2021 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Julien BRISWALTER <j1.seth@phpboost.com>
+ * @version     PHPBoost 6.0 - last update: 2021 03 08
+ * @since       PHPBoost 6.0 - 2021 03 08
+*/
+
+#####################################################
+#                       French                      #
+#####################################################
+
+$lang['the.item'] = 'l\':item';
+?>
