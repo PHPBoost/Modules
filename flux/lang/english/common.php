6f8ff861b84663defb5ffbe656d958d1ed616e3e
babsolune <babsolune@phpboost.com>
2022-01-20 07:16:04 +0100

    [FIX] categories - english

--- flux/lang/english/common.php
+++ flux/lang/english/common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 11 10
+ * @version     PHPBoost 6.0 - last update: 2022 01 20
  * @since       PHPBoost 6.0 - 2021 10 30
 */
 
@@ -47,7 +47,7 @@ $lang['flux.root.category.description'] = '
     <p>A category and a feed were created to show you how this module works. Here are some tips to get started on this module.</p>
     <ul class="formatter-ul">
     	<li class="formatter-li"> To configure or customize the module homepage your module, go into the <a class="offload" href="' . Url::to_rel(FluxUrlBuilder::configuration('flux')->relative()) . '">module administration</a></li>
-    	<li class="formatter-li"> To create categories, <a class="offload" href="' . Url::to_rel(CategoriessUrlBuilder::add()->relative(Category::ROOT_CATEGORY, 'flux')) . '">clic here</a></li>
+    	<li class="formatter-li"> To create categories, <a class="offload" href="' . Url::to_rel(CategoriesUrlBuilder::add()->relative(Category::ROOT_CATEGORY, 'flux')) . '">clic here</a></li>
     	<li class="formatter-li"> To create feeds, <a class="offload" href="' . Url::to_rel(FluxUrlBuilder::add()->relative(Category::ROOT_CATEGORY, 'flux')) . '">clic here</a></li>
     </ul>
     <p>To learn more, feel free to consult the documentation for the module on <a class="offload" href="https://www.phpboost.com">PHPBoost</a>.</p>
