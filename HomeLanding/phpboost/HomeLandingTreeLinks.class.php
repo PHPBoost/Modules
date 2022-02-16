675d8de642173fe491a53a53b2291824722331a9
babsolune <babsolune@phpboost.com>
2022-02-16 12:16:44 +0100

    [UPD] HL - treelinks order

--- HomeLanding/phpboost/HomeLandingTreeLinks.class.php
+++ HomeLanding/phpboost/HomeLandingTreeLinks.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 12 14
+ * @version     PHPBoost 6.0 - last update: 2022 02 12
  * @since       PHPBoost 5.0 - 2016 01 02
 */
 
@@ -17,7 +17,6 @@ class HomeLandingTreeLinks implements ModuleTreeLinksExtensionPoint
 		$config = HomeLandingConfig::load();
 		$sticky_title = $config->get_sticky_title();
 
-		$tree->add_link(new AdminModuleLink($lang['form.configuration'], HomeLandingUrlBuilder::configuration()));
 		$tree->add_link(new AdminModuleLink($lang['homelanding.modules.position'], HomeLandingUrlBuilder::positions()));
 
 		if(AppContext::get_current_user()->check_level(User::MODERATOR_LEVEL)){
@@ -25,6 +24,7 @@ class HomeLandingTreeLinks implements ModuleTreeLinksExtensionPoint
 			$tree->add_link(new ModuleLink($sticky_title, HomeLandingUrlBuilder::sticky()));
 		}
 
+		$tree->add_link(new AdminModuleLink($lang['form.configuration'], HomeLandingUrlBuilder::configuration()));
 		if (ModulesManager::get_module($module_id)->get_configuration()->get_documentation())
 			$tree->add_link(new AdminModuleLink($lang['form.documentation'], ModulesManager::get_module($module_id)->get_configuration()->get_documentation()));
 
