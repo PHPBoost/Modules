1761e59a2e46bb2030e4a337b5262d55b66e1892
babsolune <babsolune@phpboost.com>
2022-02-21 10:36:41 +0100

    [UPD] HL modif date

--- HomeLanding/services/modules/HomeLandingFlux.class.php
+++ HomeLanding/services/modules/HomeLandingFlux.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 12 16
+ * @version     PHPBoost 6.0 - last update: 2022 02 21
  * @since       PHPBoost 6.0 - 2021 11 14
 */
 
