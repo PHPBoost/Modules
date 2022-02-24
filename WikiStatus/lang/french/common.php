c34b50de4c3ad8c77740d4208e5d44ec424d3a95
babsolune <babsolune@phpboost.com>
2022-02-24 08:17:41 +0100

    [UPD] wikistatus - lang

--- WikiStatus/lang/french/common.php
+++ WikiStatus/lang/french/common.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 06 12
+ * @version     PHPBoost 6.0 - last update: 2022 02 24
  * @since       PHPBoost 5.0 - 2017 01 30
  * @contributor mipel <mipel@phpboost.com>
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -15,7 +15,7 @@
 
 $lang['wiki.status.module.title'] = 'Statut du Wiki';
 $lang['wiki.status.formated.module.title'] = 'Statut du Wiki';
-$lang['wiki.status.no.item.now'] = 'Aucun article du Wiki n\'a été récement modifié ou ajouté';
+$lang['wiki.status.no.item.now'] = 'Aucun article du Wiki n\'a été récemment modifié ou ajouté';
 
 $lang['wiki.status.item'] = 'Article';
 $lang['wiki.status.clue'] = 'Seuls les documents dont le statut a été déclaré (prédéfini ou personnalisé) sont affichés';
