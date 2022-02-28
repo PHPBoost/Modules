5ed628df8d2a93c51716ded70d1319fcb878c516
babsolune <babsolune@phpboost.com>
2022-02-28 04:37:32 +0100

    [UPD] HL - media

--- HomeLanding/services/modules/HomeLandingMedia.class.php
+++ HomeLanding/services/modules/HomeLandingMedia.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 02 25
+ * @version     PHPBoost 6.0 - last update: 2022 02 28
  * @since       PHPBoost 5.2 - 2020 03 06
 */
 
@@ -30,6 +30,7 @@ class HomeLandingMedia
         LEFT JOIN ' . DB_TABLE_AVERAGE_NOTES . ' notes ON notes.id_in_module = media.id AND notes.module_name = \'media\'
         LEFT JOIN ' . DB_TABLE_NOTE . ' note ON note.id_in_module = media.id AND note.module_name = \'media\' AND note.user_id = :user_id
         WHERE id_category IN :authorized_categories AND published = 2
+        ORDER BY media.creation_date DESC
         LIMIT :media_limit', array(
             'authorized_categories' => $authorized_categories,
             'user_id' => AppContext::get_current_user()->get_id(),
@@ -37,143 +38,40 @@ class HomeLandingMedia
         ));
 
         $view->put_all(array(
-			'C_NO_ITEM'       => $result->get_rows_count() == 0,
-            'C_LIST_VIEW'     => $module_config->get_display_type() == MediaConfig::LIST_VIEW,
-            'C_GRID_VIEW'     => $module_config->get_display_type() == MediaConfig::GRID_VIEW,
+			'C_ITEMS'     => $result->get_rows_count() > 0,
+            'C_LIST_VIEW' => $module_config->get_display_type() == MediaConfig::LIST_VIEW,
+            'C_GRID_VIEW' => $module_config->get_display_type() == MediaConfig::GRID_VIEW,
+
             'MODULE_NAME'     => $module_name,
             'MODULE_POSITION' => $home_config->get_module_position_by_id($module_name),
         	'ITEMS_PER_ROW'   => $module_config->get_items_per_row(),
-		    'L_MODULE_TITLE'  => ModulesManager::get_module($module_name)->get_configuration()->get_name(),
+
+            'L_MODULE_TITLE'  => ModulesManager::get_module($module_name)->get_configuration()->get_name(),
         ));
 
         while ($row = $result->fetch())
         {
-            $mime_type_tpl = $row['mime_type'];
+            $thumbnail = new Url($row['thumbnail']);
+            $thumbnail_url = $thumbnail->rel();
 
-            if ($mime_type_tpl == 'video/host')
-            {
-                $poster = new Url($row['thumbnail']);
-                $pathinfo = pathinfo($row['file_url']);
-                $media_id = $pathinfo['basename'];
+            $summary = TextHelper::cut_string(@strip_tags(FormatingHelper::second_parse($row['content']), '<br><br/>'), $module_config->get_characters_number_to_cut());
 
-            	if(strpos($pathinfo['dirname'], 'youtu') !== false)
-            	{
-            		$watch = 'watch?v=';
-            	    if(strpos($media_id, $watch) !== false) {
-        				$media_id = substr_replace($media_id, '', 0, 8);
-        				list($media_id) = explode('&', $media_id);
-                    }
-                    $player = 'https://www.youtube.com/embed/';
-            	}
-            	if(strpos($pathinfo['dirname'], 'vimeo') !== false)
-            	{
-        			$player = 'https://player.vimeo.com/video/';
+            $view->assign_block_vars('items', array(
+                'C_SUMMARY'   => !empty($row['content']),
+                'C_AUDIO'     => $row['mime_type'] == 'audio/mpeg',
+                'C_THUMBNAIL' => !empty($row['thumbnail']),
 
-            	}
-            	if(strpos($pathinfo['dirname'], 'dailymotion') !== false)
-            	{
-        			$player = 'https://www.dailymotion.com/embed/video/';
-            	}
-                if(strpos($pathinfo['dirname'], 'odysee') !== false)
-                {
-                    $explode = explode('/', $pathinfo['dirname']);
-			        $media_id = $explode[5] . '/' . $media_id;
-                    $player = 'https://odysee.com/$/embed/';
-                }
-                if(strpos($pathinfo['dirname'], MediaConfig::load()->get_peertube_constant()) !== false)
-                {
-                    $peertube_link = MediaConfig::load()->get_peertube_constant();
-                    $peertube_host = explode('/', $peertube_link);
-                    $peertube_host_player = explode('.', $peertube_host[2]);
-                    $sliced_name = array_slice($peertube_host_player, 0, -1);
-                    $player = implode('.', $sliced_name);
-                    $player = $peertube_link . '/videos/embed/';
-                }
-                if(strpos($pathinfo['dirname'], 'twitch') !== false)
-                {
-        			$parent = pathinfo(GeneralConfig::load()->get_site_url());
-        			$parent = $parent['basename'];
-			        $media_id = $media_id . '&parent=' . $parent;
-                    $player = 'https://player.twitch.tv/?video=';
-                }
+                'PSEUDO'        => $row['display_name'],
+                'TITLE'         => $row['title'],
+                'ID'            => $row['id'],
+                'DATE'          => Date::to_format($row['creation_date'], Date::FORMAT_DAY_MONTH_YEAR),
+                'SORT_DATE'     => $row['creation_date'],
+                'CATEGORY_NAME' => $row['id_category'] == Category::ROOT_CATEGORY ? $module_lang['common.root'] : CategoriesService::get_categories_manager('media')->get_categories_cache()->get_category($row['id_category'])->get_name(),
+                'SUMMARY'       => FormatingHelper::second_parse($summary),
 
-                $view->assign_block_vars('media_host', array(
-                    'PSEUDO'        => $row['display_name'],
-                    'TITLE'         => $row['title'],
-                    'ID'            => $row['id'],
-                    'DATE'          => Date::to_format($row['creation_date'], Date::FORMAT_DAY_MONTH_YEAR),
-                    'SORT_DATE'     => $row['creation_date'],
-                    'C_POSTER'      => !empty($poster),
-                    'POSTER'        => $poster->rel(),
-                    'PLAYER'        => $player,
-                    'CATEGORY_NAME' => $row['id_category'] == Category::ROOT_CATEGORY ? $module_lang['common.root'] : CategoriesService::get_categories_manager('media')->get_categories_cache()->get_category($row['id_category'])->get_name(),
-
-                    'U_ITEM'   => Url::to_rel('/media/' . url('media.php?id=' . $row['id'], 'media-' . $row['id'] . '-' . $row['id_category'] . '+' . Url::encode_rewrite($row['title']) . '.php')),
-                    'MEDIA_ID' => $media_id,
-                    'WIDTH'    => $row['width'],
-                    'HEIGHT'   => $row['height']
-                ));
-            }
-            elseif ($mime_type_tpl == 'video/mp4')
-            {
-                $poster = new Url($row['thumbnail']);
-                $view->assign_block_vars('media_mp4', array(
-                    'PSEUDO'        => $row['display_name'],
-                    'TITLE'         => $row['title'],
-                    'ID'            => $row['id'],
-                    'DATE'          => Date::to_format($row['creation_date'], Date::FORMAT_DAY_MONTH_YEAR),
-                    'SORT_DATE'     => $row['creation_date'],
-                    'C_POSTER'      => !empty($poster),
-                    'POSTER'        => $poster->rel(),
-                    'CATEGORY_NAME' => $row['id_category'] == Category::ROOT_CATEGORY ? $module_lang['common.root'] : CategoriesService::get_categories_manager('media')->get_categories_cache()->get_category($row['id_category'])->get_name(),
-
-                    'U_ITEM'   => Url::to_rel('/media/' . url('media.php?id=' . $row['id'], 'media-' . $row['id'] . '-' . $row['id_category'] . '+' . Url::encode_rewrite($row['title']) . '.php')),
-                    'FILE_URL' => Url::to_rel($row['file_url']),
-                    'MIME'     => $row['mime_type'],
-                    'WIDTH'    => $row['width'],
-                    'HEIGHT'   => $row['height']
-                ));
-            }
-            elseif ($mime_type_tpl == 'audio/mpeg')
-            {
-                $poster = new Url($row['thumbnail']);
-                $view->assign_block_vars('media_mp3', array(
-                    'PSEUDO'        => $row['display_name'],
-                    'TITLE'         => $row['title'],
-                    'ID'            => $row['id'],
-                    'DATE'          => strftime('%d/%m/%Y', $row['creation_date']),
-                    'SORT_DATE'     => Date::to_format($row['creation_date'], Date::FORMAT_DAY_MONTH_YEAR),
-                    'C_POSTER'      => !empty($poster),
-                    'POSTER'        => $poster->rel(),
-                    'CATEGORY_NAME' => $row['id_category'] == Category::ROOT_CATEGORY ? $module_lang['common.root'] : CategoriesService::get_categories_manager('media')->get_categories_cache()->get_category($row['id_category'])->get_name(),
-
-                    'U_ITEM'   => Url::to_rel('/media/' . url('media.php?id=' . $row['id'], 'media-' . $row['id'] . '-' . $row['id_category'] . '+' . Url::encode_rewrite($row['title']) . '.php')),
-                    'FILE_URL' => Url::to_rel($row['file_url']),
-                    'MIME'     => $row['mime_type'],
-                    'WIDTH'    => $row['width'],
-                    'HEIGHT'   => $row['height']
-                ));
-            }
-            else
-            {
-                $poster = new Url($row['thumbnail']);
-                $view->assign_block_vars('media_other', array(
-                    'PSEUDO'        => $row['display_name'],
-                    'TITLE'         => $row['title'],
-                    'ID'            => $row['id'],
-                    'DATE'          => Date::to_format($row['creation_date'], Date::FORMAT_DAY_MONTH_YEAR),
-                    'SORT_DATE'     => $row['creation_date'],
-                    'C_POSTER'      => !empty($poster),
-                    'POSTER'        => $poster->rel(),
-                    'CATEGORY_NAME' => $row['id_category'] == Category::ROOT_CATEGORY ? $module_lang['common.root'] : CategoriesService::get_categories_manager('media')->get_categories_cache()->get_category($row['id_category'])->get_name(),
-
-                    'U_ITEM' => Url::to_rel('/media/' . url('media.php?id=' . $row['id'], 'media-' . $row['id'] . '-' . $row['id_category'] . '+' . Url::encode_rewrite($row['title']) . '.php')),
-                    'FILE_URL' => Url::to_rel($row['file_url']),
-                    'MIME' => $row['mime_type'],
-                    'WIDTH' => $row['width'],
-                    'HEIGHT' => $row['height']
-                ));
-            }
+                'U_THUMBNAIL' => $thumbnail_url,
+                'U_ITEM'      => Url::to_rel('/media/' . url('media.php?id=' . $row['id'], 'media-' . $row['id'] . '-' . $row['id_category'] . '+' . Url::encode_rewrite($row['title']) . '.php')),
+            ));
         }
         $result->dispose();
 
--- HomeLanding/templates/pagecontent/media.tpl
+++ HomeLanding/templates/pagecontent/media.tpl
@@ -10,113 +10,44 @@
 				</div>
 			</header>
 
-			# IF C_NO_ITEM #
+			# IF C_ITEMS #
+				<div class="cell-flex cell-columns-{ITEMS_PER_ROW}" data-type="content" data-listorder-group="media-items">
+					# START items #
+						<div class="cell" itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
+							<header class="cell-header">
+								<h3 class="cell-name"><a class="offload" href="{items.U_ITEM}">{items.TITLE}</a></h3>
+								<i class="fa fa-# IF items.C_AUDIO #music# ELSE #film# ENDIF #" aria-hidden="true"></i>
+							</header>
+							<div class="cell-body">
+								<div class="cell-infos">
+									<div class="more">
+										<span class="pinned item-author"><i class="fa fa-fw fa-user"></i> {items.PSEUDO}</span>
+										<span class="pinned item-creation-date"><i class="far fa-fw fa-calendar-alt"></i> {items.DATE}</span>
+										<span class="pinned item-category"><i class="far fa-fw fa-folder"></i> {items.CATEGORY_NAME}</span>
+									</div>
+								</div>
+								# IF items.C_THUMBNAIL #
+									<div class="cell-thumbnail cell-landscape cell-center">
+										<img src="{items.U_THUMBNAIL}" alt="{items.TITLE}">
+										<a class="cell-thumbnail-caption offload" href="{items.U_ITEM}"><i class="fa fa-2x fa-play-circle" aria-hidden="true"></i></a>
+									</div>
+								# ENDIF #
+								# IF items.C_SUMMARY #
+									<div class="cell-content">
+										{items.SUMMARY}
+									</div>
+								# ENDIF #
+							</div>
+						</div>
+					# END items #
+				</div>
+			# ELSE #
 				<div class="content">
 					<div class="message-helper bgc notice">
 						{@common.no.item.now}
 					</div>
 				</div>
-			# ELSE #
-				<div
-					class="hidden"
-					data-listorder-control="hidden-sort"
-					data-group="media-items"
-					data-path=".lo-date-media"
-					data-order="desc"
-					data-type="number">
-				</div>
-				<div class="content cell-flex cell-columns-{ITEMS_PER_ROW}" data-type="content" data-listorder-group="media-items">
-					# START media_host #
-						<div data-listorder-item class="cell" itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
-							<span class="lo-date-media hidden">{media_host.SORT_DATE}</span>
-							<header class="cell-header">
-								<h3 class="cell-name"><a class="offload" href="{media_host.U_ITEM}">{media_host.TITLE}</a></h3>
-							</header>
-							<div class="cell-body">
-								<div class="cell-infos">
-									<div class="more">
-										<span class="pinned item-author"><i class="fa fa-fw fa-user"></i> {media_host.PSEUDO}</span>
-										<span class="pinned item-creation-date"><i class="far fa-fw fa-calendar-alt"></i> {media_host.DATE}</span>
-										<span class="pinned item-category"><i class="far fa-fw fa-folder"></i> {media_host.CATEGORY_NAME}</span>
-									</div>
-								</div>
-								<div class="cell-thumbnail cell-landscape cell-center">
-									<img src="{media_host.POSTER}" alt="{media_host.TITLE}">
-									<a class="cell-thumbnail-caption offload" href="{media_host.U_ITEM}"><i class="fa fa-2x fa-play-circle" aria-hidden="true"></i></a>
-								</div>
-							</div>
-						</div>
-					# END media_host #
-
-					# START media_mp4 #
-						<div data-listorder-item class="cell" itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
-							<span class="lo-date-media hidden">{media_mp4.SORT_DATE}</span>
-							<header class="cell-header">
-								<h3 class="cell-name"><a class="offload" href="{media_mp4.U_ITEM}">{media_mp4.TITLE}</a></h3>
-							</header>
-							<div class="cell-body">
-								<div class="cell-infos">
-									<div class="more">
-										<span class="pinned item-author"><i class="fa fa-fw fa-user"></i> {media_mp4.PSEUDO}</span>
-										<span class="pinned item-creation-date"><i class="far fa-fw fa-calendar-alt"></i> {media_mp4.DATE}</span>
-										<span class="pinned item-category"><i class="far fa-fw fa-folder"></i> {media_mp4.CATEGORY_NAME}</span>
-									</div>
-								</div>
-								<div class="cell-thumbnail cell-landscape cell-center">
-									<img src="{media_mp4.POSTER}" alt="{media_mp4.TITLE}">
-									<a class="cell-thumbnail-caption offload" href="{media_mp4.U_ITEM}"><i class="fa fa-2x fa-play-circle" aria-hidden="true"></i></a>
-								</div>
-							</div>
-						</div>
-					# END media_mp4 #
-
-					# START media_mp3 #
-						<div data-listorder-item class="cell" itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
-							<span class="lo-date-media hidden">{media_mp3.SORT_DATE}</span>
-							<header class="cell-header">
-								<h3 class="cell-name"><a class="offload" href="{media_mp3.U_ITEM}">{media_mp3.TITLE}</a></h3>
-							</header>
-							<div class="cell-body">
-								<div class="cell-infos">
-									<div class="more">
-										<span class="pinned item-author"><i class="fa fa-fw fa-user"></i> {media_mp3.PSEUDO}</span>
-										<span class="pinned item-creation-date"><i class="far fa-fw fa-calendar-alt"></i> {media_mp3.DATE}</span>
-										<span class="pinned item-category"><i class="far fa-fw fa-folder"></i> {media_mp3.CATEGORY_NAME}</span>
-									</div>
-								</div>
-								<div class="cell-thumbnail cell-landscape cell-center">
-									<img src="{media_mp3.POSTER}" alt="{media_mp3.TITLE}">
-									<a class="cell-thumbnail-caption offload" href="{media_mp3.U_ITEM}"><i class="fa fa-2x fa-music" aria-hidden="true"></i></a>
-								</div>
-							</div>
-						</div>
-					# END media_mp3 #
-
-					# START media_other #
-						<div data-listorder-item class="cell" itemscope="itemscope" itemtype="https://schema.org/CreativeWork">
-							<span class="lo-date-media hidden">{media_other.SORT_DATE}</span>
-							<header class="cell-header">
-								<h3 class="cell-name"><a class="offload" href="{media_other.U_ITEM}">{media_other.TITLE}</a></h3>
-							</header>
-							<div class="cell-body">
-								<div class="cell-infos">
-									<div class="more">
-										<span class="pinned item-author"><i class="fa fa-fw fa-user"></i> {media_other.PSEUDO}</span>
-										<span class="pinned item-creation-date"><i class="far fa-fw fa-calendar-alt"></i> {media_other.DATE}</span>
-										<span class="pinned item-category"><i class="far fa-fw fa-folder"></i> {media_other.CATEGORY_NAME}</span>
-									</div>
-								</div>
-								<div class="cell-thumbnail cell-landscape cell-center">
-									<img src="{media_other.POSTER}" alt="{media_other.TITLE}">
-									<a class="cell-thumbnail-caption offload" href="{media_other.U_ITEM}"><i class="fa fa-2x fa-play-circle" aria-hidden="true"></i></a>
-								</div>
-							</div>
-						</div>
-					# END media_other #
-
-				</div>
 			# ENDIF #
-			<footer></footer>
 		</article>
 
 	</div>
