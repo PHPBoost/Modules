78e549b9b2adc4823e8dbd6a61ceb105087434b0
babsolune <babsolune@phpboost.com>
2022-03-30 05:12:47 +0200

    [UPD] update to modules

--- birthday/update/BirthdayModuleUpdateVersion.class.php
+++ birthday/update/BirthdayModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2022 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2021 07 04
+ * @version     PHPBoost 6.0 - last update: 2022 03 30
  * @since       PHPBoost 6.0 - 2019 12 27
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
@@ -17,6 +17,7 @@ class BirthdayModuleUpdateVersion extends ModuleUpdateVersion
 		self::$delete_old_files_list = array(
 			'/lang/english/config.php',
 			'/lang/french/config.php',
+			'/templates/birthday.css',
 		);
 
 		self::$delete_old_folders_list = array(
