6c5b94c031acb30a397abecdf012fbdb4841bb96
babsolune <babsolune@phpboost.com>
2023-01-07 13:55:09 +0100

    [FIX] broadcast - no publications

--- broadcast/phpboost/BroadcastExtensionPointProvider.class.php
+++ broadcast/phpboost/BroadcastExtensionPointProvider.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 10 25
+ * @version     PHPBoost 6.0 - last update: 2023 01 07
  * @since       PHPBoost 6.0 - 2022 10 25
  */
 
@@ -12,6 +12,11 @@ class BroadcastExtensionPointProvider extends ItemsModuleExtensionPointProvider
 	public function home_page()
 	{
 		return new DefaultHomePageDisplay($this->get_id(), BroadcastCategoryController::get_view());
+	}
+
+	public function user()
+	{
+		return false;
 	}
 }
 ?>
