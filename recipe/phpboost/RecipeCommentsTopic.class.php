bc06384716206a0de55c8ed98f84df7675cde046
j1.seth <julien.briswalter@gmail.com>
2023-01-16 00:25:50 +0100

    [FIX] Recipe comments

--- recipe/phpboost/RecipeCommentsTopic.class.php
+++ recipe/phpboost/RecipeCommentsTopic.class.php
@@ -3,39 +3,22 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 10 17
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
  * @since       PHPBoost 6.0 - 2022 08 26
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  */
 
-class RecipeCommentsTopic extends CommentsTopic
+class RecipeCommentsTopic extends DefaultCommentsTopic
 {
-	private $item;
-
 	public function __construct(RecipeItem $item = null)
 	{
 		parent::__construct('recipe');
 		$this->item = $item;
 	}
 
-	public function get_authorizations()
+	protected function get_item_from_manager()
 	{
-		$authorizations = new CommentsAuthorizations();
-		$authorizations->set_authorized_access_module(CategoriesAuthorizationsService::check_authorizations($this->get_item()->get_id_category())->read());
-		return $authorizations;
-	}
-
-	public function is_displayed()
-	{
-		return $this->get_item()->is_published();
-	}
-
-	private function get_item()
-	{
-		if ($this->item === null)
-		{
-			$this->item = RecipeService::get_item($this->get_id_in_module());
-		}
-		return $this->item;
+		return RecipeService::get_item($this->get_id_in_module());
 	}
 }
 ?>
--- recipe/phpboost/RecipeExtensionPointProvider.class.php
+++ recipe/phpboost/RecipeExtensionPointProvider.class.php
@@ -3,12 +3,18 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 10 17
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
  * @since       PHPBoost 6.0 - 2022 08 26
+ * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
  */
 
 class RecipeExtensionPointProvider extends ItemsModuleExtensionPointProvider
 {
+	public function comments()
+	{
+		return new CommentsTopics(array(new RecipeCommentsTopic()));
+	}
+
 	public function home_page()
 	{
 		return new DefaultHomePageDisplay($this->get_id(), RecipeCategoryController::get_view());
