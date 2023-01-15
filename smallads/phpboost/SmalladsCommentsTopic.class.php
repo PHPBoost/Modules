6f9aa7232b4c9887ce080b6e318d3e59e0f4222b
j1.seth <julien.briswalter@gmail.com>
2023-01-16 00:20:55 +0100

    [FIX] Smallads comments

--- smallads/phpboost/SmalladsCommentsTopic.class.php
+++ smallads/phpboost/SmalladsCommentsTopic.class.php
@@ -3,40 +3,22 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 04 14
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
  * @since       PHPBoost 5.1 - 2018 03 15
  * @contributor Julien BRISWALTER <j1.seth@phpboost.com>
 */
 
-class SmalladsCommentsTopic extends CommentsTopic
+class SmalladsCommentsTopic extends DefaultCommentsTopic
 {
-	private $item;
-
 	public function __construct(SmalladsItem $item = null)
 	{
 		parent::__construct('smallads');
 		$this->item = $item;
 	}
 
-	public function get_authorizations()
+	protected function get_item_from_manager()
 	{
-		$authorizations = new CommentsAuthorizations();
-		$authorizations->set_authorized_access_module(CategoriesAuthorizationsService::check_authorizations($this->get_item()->get_id_category(), 'smallads')->read());
-		return $authorizations;
-	}
-
-	public function is_displayed()
-	{
-		return $this->get_item()->is_published();
-	}
-
-	private function get_item()
-	{
-		if ($this->item === null)
-		{
-			$this->item = SmalladsService::get_item($this->get_id_in_module());
-		}
-		return $this->item;
+		return SmalladsService::get_item($this->get_id_in_module());
 	}
 }
 ?>
--- smallads/phpboost/SmalladsExtensionPointProvider.class.php
+++ smallads/phpboost/SmalladsExtensionPointProvider.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2020 12 08
+ * @version     PHPBoost 6.0 - last update: 2023 01 16
  * @since       PHPBoost 4.0 - 2013 01 29
  * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
  * @contributor xela <xela@phpboost.com>
@@ -11,6 +11,11 @@
 
 class SmalladsExtensionPointProvider extends ItemsModuleExtensionPointProvider
 {
+	public function comments()
+	{
+		return new CommentsTopics(array(new SmalladsCommentsTopic()));
+	}
+
 	public function home_page()
 	{
 		return new DefaultHomePageDisplay($this->get_id(), SmalladsCategoryController::get_view());
