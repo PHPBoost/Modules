54fd4a1f76277ed74f960a2c97c6d6f1712b6176
babsolune <babsolune@phpboost.com>
2023-01-27 10:08:30 +0100

    [FIX] broadcast - search

--- broadcast/phpboost/BroadcastModuleMiniMenu.class.php
+++ broadcast/phpboost/BroadcastModuleMiniMenu.class.php
@@ -5,7 +5,7 @@
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
  * @version     PHPBoost 6.0 - last update: 2022 10 17
- * @since       PHPBoost 4.1 - 2017 02 21
+ * @since       PHPBoost 6.0 - 2022 10 25
  */
 
 class BroadcastModuleMiniMenu extends ModuleMiniMenu
--- broadcast/phpboost/BroadcastSearchable.class.php
+++ broadcast/phpboost/BroadcastSearchable.class.php
@@ -4,8 +4,8 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 10 17
- * @since       PHPBoost 4.1 - 2017 02 21
+ * @version     PHPBoost 6.0 - last update: 2023 01 27
+ * @since       PHPBoost 6.0 - 2022 10 25
  */
 
 class BroadcastSearchable extends DefaultSearchable
@@ -17,17 +17,11 @@ class BroadcastSearchable extends DefaultSearchable
 
 		$this->table_name = BroadcastSetup::$broadcast_table;
 
-		$this->authorized_categories = CategoriesService::get_authorized_categories(Category::ROOT_CATEGORY, BroadcastConfig::load()->are_descriptions_displayed_to_guests(), $module_id);
-
 		$this->field_title = 'title';
 		$this->field_rewrited_title = 'rewrited_title';
 		$this->field_content = 'content';
 
 		$this->field_published = 'published';
-
-		$this->has_validation_period = true;
-		$this->field_validation_start_time = 'start_time';
-		$this->field_validation_end_time = 'end_time';
 	}
 }
 ?>
