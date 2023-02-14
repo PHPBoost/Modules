d365f49f04540fd3f9c6420d49ccc265bcbf4430
babsolune <babsolune@phpboost.com>
2023-02-14 08:06:48 +0100

    [FIX] recipe - count items

--- recipe/services/RecipeCategoriesCache.class.php
+++ recipe/services/RecipeCategoriesCache.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2022 10 17
+ * @version     PHPBoost 6.0 - last update: 2023 02 14
  * @since       PHPBoost 6.0 - 2022 08 26
  */
 
@@ -16,7 +16,13 @@ class RecipeCategoriesCache extends DefaultRichCategoriesCache
 
 	protected function get_category_elements_number($id_category)
 	{
-		return 0;
+		$now = new Date();
+		return RecipeService::count('WHERE id_category = :id_category AND (published = 1 OR (published = 2 AND publishing_start_date < :timestamp_now AND (publishing_end_date > :timestamp_now OR publishing_end_date = 0)))',
+			array(
+				'timestamp_now' => $now->get_timestamp(),
+				'id_category' => $id_category
+			)
+		);
 	}
 
 	protected function get_root_category_authorizations()
@@ -26,7 +32,10 @@ class RecipeCategoriesCache extends DefaultRichCategoriesCache
 	
 	protected function get_root_category_description()
 	{
-		return StringVars::replace_vars(LangLoader::get_message('recipe.seo.description.root', 'common', 'recipe'), array('site' => GeneralConfig::load()->get_site_name()));
+		$description = RecipeConfig::load()->get_root_category_description();
+		if (empty($description))
+            $description = StringVars::replace_vars(LangLoader::get_message('recipe.seo.description.root', 'common', 'recipe'), array('site' => GeneralConfig::load()->get_site_name()));
+        return $description;
 	}
 }
 ?>
