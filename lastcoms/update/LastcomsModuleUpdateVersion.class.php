51a058e7a250076dc768c931d9174b0481dd198d
babsolune <babsolune@phpboost.com>
2023-06-19 09:42:14 +0200

    [UPD] file name update

--- lastcoms/update/LastcomsModuleUpdateVersion.class.php
+++ lastcoms/update/LastcomsModuleUpdateVersion.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2019 12 28
+ * @version     PHPBoost 6.0 - last update: 2023 06 19
  * @since       PHPBoost 6.0 - 2019 12 27
 */
 
@@ -12,6 +12,11 @@ class LastcomsModuleUpdateVersion extends ModuleUpdateVersion
 	public function __construct()
 	{
 		parent::__construct('lastcoms');
+
+		self::$delete_old_files_list = array(
+            '/phpboost/LastcomsSetup.php'
+        );
+
 		self::$delete_old_folders_list = array(
 			'/util'
 		);
