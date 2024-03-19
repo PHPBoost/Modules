31eb178e0dc156ef2a17acd6edd2bacb2aad4aac
babsolune <babsolune@phpboost.com>
2024-03-19 10:38:58 +0100

    [UPD] setup

--- dictionary/phpboost/DictionarySetup.class.php
+++ dictionary/phpboost/DictionarySetup.class.php
@@ -3,13 +3,16 @@
  * @copyright   &copy; 2005-2023 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2018 12 24
+ * @version     PHPBoost 6.0 - last update: 2023 07 17
  * @since       PHPBoost 3.0 - 2012 11 15
  * @contributor mipel <mipel@phpboost.com>
+ * @contributor Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
 class DictionarySetup extends DefaultModuleSetup
 {
+	private $messages;
+	private $querier;
 	public static $dictionary_table;
 	public static $dictionary_cat_table;
 
