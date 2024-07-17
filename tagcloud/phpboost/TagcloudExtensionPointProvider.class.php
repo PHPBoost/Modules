8b4b2cd764f9892e24ba5bf6ca772c917ceeda42
babsolune <babsolune@phpboost.com>
2024-07-17 10:16:35 +0200

    [UPD] tagcloud | js cache & array

--- tagcloud/phpboost/TagcloudExtensionPointProvider.class.php
+++ tagcloud/phpboost/TagcloudExtensionPointProvider.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2024 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.0 - last update: 2023 01 17
+ * @version     PHPBoost 6.0 - last update: 2024 07 17
  * @since       PHPBoost 6.0 - 2023 01 17
 */
 
@@ -16,7 +16,14 @@ class TagcloudExtensionPointProvider extends ExtensionPointProvider
 
     public function menus()
     {
-        return new ModuleMenus(array(new TagcloudModuleMiniMenu()));
+        return new ModuleMenus([new TagcloudModuleMiniMenu()]);
     }
+
+	public function js_files()
+	{
+		$js_file = new ModuleJsFiles();
+		$js_file->adding_always_displayed_file('tagcloud.js');
+		return $js_file;
+	}
 }
 ?>
---
+++ tagcloud/phpboost/TagcloudSetup.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2023 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 6.0 - last update: 2023 01 17
+ * @since       PHPBoost 6.0 - 2023 01 17
+*/
+
+class TagcloudSetup extends DefaultModuleSetup
+{
+	public function upgrade($installed_version)
+	{
+		return '6.0.0';
+	}
+
+	public function uninstall()
+	{
+		ConfigManager::delete('tagcloud', 'config');
+	}
+}
+?>
