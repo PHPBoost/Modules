acdacd02d66ef87e679ccf924e04f3ca814072b5
babsolune <babsolune@phpboost.com>
2026-07-01 17:33:10 +0200

    [UPD] pages - installed page

--- pages/lang/english/install.php
+++ pages/lang/english/install.php
@@ -20,9 +20,63 @@ $lang['categories'][] = [
 ];
 
 $lang['items'][] = [
-	'item.title'   => 'A first page',
-	'item.content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem vel odio eligendi, esse nostrum quo sunt consectetur ea earum laudantium voluptate sequi harum,
-    sint voluptates cumque, minima a in sit. Accusantium eum a, rerum libero fugiat quae vero sequi officia!',
+	'item.title'   => 'Legal mentions',
+	'item.content' => '
+        <h2 class="formatter-title">Site manager</h2>
+        <br />
+        Release Manager: <a class="offload" href="/#">[ManagerName]</a>
+        <br />
+        Creation and development: ' . GeneralConfig::load()->get_site_name() . '
+        <br />
+        For any request, please contact:
+        <br />
+        email: [LinkToEmail/Form]
+        <br/><br/>
+        <h2 class="formatter-title">Property right</h2>
+        <br />
+        All text, photos and illustrations on this site are the property of ' . GeneralConfig::load()->get_site_name(). ' and its contractual partners.
+        Any representation, reproduction in whole or in part made without the consent of the owner is illegal.
+        If you wish to reproduce photos or documents contained in this site, solely for non-profit purposes, you must obtain prior written authorization from before any distribution ' . GeneralConfig::load()->get_site_name() . '.
+        For this: send a written request by email to ' . GeneralConfig::load()->get_site_name() . ' specifying the exact content of what you wish to reproduce.
+        After approval, you will be asked to cite the sources precisely. If you are a journalist and would like to obtain a high-definition photo for a press release, send us a request through the same channel: [LinkToEmail/Form]
+        <br/><br/>
+        <h2 class="formatter-title">Site Hosting</h2>
+        <br />
+        [HostMailingAddress]
+        <br />
+        <h2 class="formatter-title">Third Party Apps</h2>
+        <br />
+        PHP and JavaScript scripts are integrated into this site in compliance with the rights granted by their authors. The mentions provided for in these are visible in the page source codes. <br />
+        <br />
+        <h2 class="formatter-title">Personal data protection</h2>
+        <br />
+        The site does not collect any personal data
+        <br/><br/>
+        <h2 class="formatter-title">Cookies</h2>
+        <br />
+        When browsing certain parts of the site, cookies may be imported onto your computer.
+        These do not allow you to be identified, but they record information relating to navigation that the program can read during your subsequent visits.
+        You can prevent the storage of cookies by configuring your internet browser.
+        <br />
+        <h2 class="formatter-title">Links to this site</h2>
+        <br />
+        Links to this site are welcome under the following conditions:
+        <br />
+        <ul class="formatter-ul">
+            <li class="formatter-li">Respect for reciprocity.</li>
+            <li class="formatter-li">Link on a page of your site that is indexed by search engines.</li>
+            <li class="formatter-li">Full page opening <a class="offload" href="/">' . GeneralConfig::load()->get_site_name() . '</a> in a browser, without any site-specific frames or endorsements. </li>
+        </ul>
+        <br/><br/>
+        <h2 class="formatter-title">Credits</h2><br />
+        The content included in this site remains the intellectual property of their respective authors:
+        <br />
+        <ul class="formatter-ul">
+            <li class="formatter-li">Photo credits: ' . GeneralConfig::load()->get_site_name() . '</li>
+            <li class="formatter-li">Illustration credits: ' . GeneralConfig::load()->get_site_name() . '</li>
+            <li class="formatter-li">Text credits: ' . GeneralConfig::load()->get_site_name() . '</li>
+            <li class="formatter-li">For any request to reproduce content: [LinkToEmail/Form]</li>
+        </ul>',
 	'item.summary' => ''
 ];
 ?>
--- pages/lang/french/install.php
+++ pages/lang/french/install.php
@@ -20,9 +20,63 @@ $lang['categories'][] = [
 ];
 
 $lang['items'][] = [
-	'item.title'   => 'Une première page',
-	'item.content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem vel odio eligendi, esse nostrum quo sunt consectetur ea earum laudantium voluptate sequi harum,
-    sint voluptates cumque, minima a in sit. Accusantium eum a, rerum libero fugiat quae vero sequi officia!',
-	'item.summary' => ''
+
+    'item.title' => 'Mentions légales',
+    'item.content' => '
+        <h2 class="formatter-title">Responsable du site</h2>
+        <br />
+        Responsable de Publication : <a class="offload" href="/#">[NomDuResponsable]</a>
+        <br />
+        Création et développement : ' . GeneralConfig::load()->get_site_name() . '
+        <br />
+        Pour toute demande, adressez-vous à :
+        <br />
+        email: [LienVersEmail/FormulaireDeContact]
+        <br /><br />
+        <h2 class="formatter-title">Droit de propriété</h2>
+        <br />
+        Tous les textes, photos et illustrations présents sur ce site sont la propriété de ' . GeneralConfig::load()->get_site_name() . ' et ses partenaires contractuels.
+        Toute représentation, reproduction intégrale ou partielle faite sans le consentement du propriétaire est illicite.
+        Si vous souhaitez reproduire des photos ou documents contenus dans ce site, uniquement à des fins non lucratives, vous devez avant toute diffusion obtenir l\'autorisation écrite préalable à ' . GeneralConfig::load()->get_site_name() . '.
+        Pour cela : adressez une demande écrite par e-mail à ' . GeneralConfig::load()->get_site_name() . ' en précisant le contenu exact de ce que vous souhaitez reproduire.
+        Après accord, il vous sera demandé de citer précisément les sources. Si vous êtes journaliste et que vous souhaitez obtenir une photo en haute définition pour une parution presse, adressez nous une demande par ce même canal: [LienVersEmail/FormulaireDeContact]
+        <br /><br />
+        <h2 class="formatter-title">Hébergement du site</h2>
+        <br />
+        [AdressePostaleDeLHebergeur]
+        <br />
+        <h2 class="formatter-title">Applications tierces</h2>
+        <br />
+        Des scripts php et javascripts sont intégrés à ce site dans le respect des droits concédés par leurs auteurs. Les mentions prévues par ceux-ci sont visibles dans les codes sources des pages.<br />
+        <br />
+        <h2 class="formatter-title">Protection des données personnelles</h2>
+        <br />
+        Le site ne récolte aucune donnée personnelle
+        <br /><br />
+        <h2 class="formatter-title">Cookies</h2>
+        <br />
+        Lors de la navigation dans certains endroits du site, des cookies peuvent être importés sur votre ordinateur.
+        Ceux-ci ne permettent pas de vous identifier, mais ils enregistrent des informations relatives à la navigation que le programme pourra lire lors de vos visites ultérieures.
+        Vous pouvez vous opposer à l\'enregistrement de cookies en configurant votre navigateur internet.
+        <br />
+        <h2 class="formatter-title">Liens vers ce site</h2>
+        <br />
+        Les liens vers ce site sont bienvenus aux conditions suivantes :
+        <br />
+        <ul class="formatter-ul">
+            <li class="formatter-li">Respect de la réciprocité.</li>
+            <li class="formatter-li">Lien sur une page de votre site indexée par les moteurs de recherche.</li>
+            <li class="formatter-li">Ouverture intégrale de la page <a class="offload" href="/">' . GeneralConfig::load()->get_site_name() . '</a> dans un navigateur, sans cadre ni mentions propres à votre site.</li>
+        </ul>
+        <br /><br />
+        <h2 class="formatter-title">Crédits</h2><br />
+        Les contenus intégrés dans ce site reste la propriété intellectuelle de leurs auteurs respectifs :
+        <br />
+        <ul class="formatter-ul">
+            <li class="formatter-li">Crédits photos : ' . GeneralConfig::load()->get_site_name() . '</li>
+            <li class="formatter-li">Crédits illustrations : ' . GeneralConfig::load()->get_site_name() . '</li>
+            <li class="formatter-li">Crédits textes : ' . GeneralConfig::load()->get_site_name() . '</li>
+            <li class="formatter-li">Pour toute demande de reproduction d\'un contenu : [LienVersEmail/FormulaireDeContact]</li>
+        </ul>'
 ];
 ?>
