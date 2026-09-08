5051ccedc1a729a9f57a6a04d4a9d4a8d975e536
babsolune <babsolune@phpboost.com>
2026-09-08 11:53:18 +0200

    [FIX]  spots home page

--- spots/services/SpotsItem.class.php
+++ spots/services/SpotsItem.class.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2026 05 19
+ * @version     PHPBoost 6.1 - last update: 2026 09 08
  * @since       PHPBoost 6.0 - 2021 08 22
 */
 
@@ -488,7 +488,12 @@ class SpotsItem
 
 		$root_category = $category->get_id() == Category::ROOT_CATEGORY;
 
-        $category_address_values = (ModulesManager::is_module_activated('GoogleMaps') && TextHelper::deserialize($root_category)) ? GoogleMapsConfig::load()->get_default_marker_address() : $category->get_category_address();
+		$category_address = '';
+		if (ModulesManager::is_module_activated('GoogleMaps'))
+		{
+			$category_address = $root_category ? GoogleMapsConfig::load()->get_default_marker_address() : $category->get_category_address();
+		}
+		$category_address_values = TextHelper::deserialize($category_address);
 
         return array_merge(
 			Date::get_array_tpl_vars($this->creation_date, 'date'),
@@ -528,19 +533,19 @@ class SpotsItem
                 'CATEGORY_COLOR'   	   => !$root_category ? $category->get_color() : $config->get_default_color(),
                 'CATEGORY_INNER_ICON'  => !$root_category ? (!empty($category->get_inner_icon()) ? $category->get_inner_icon() : $config->get_default_inner_icon()) : $config->get_default_inner_icon(),
                 'CATEGORY_LATITUDE'    => 
-                    ModulesManager::is_module_activated('GoogleMaps') && TextHelper::is_serialized($root_category)
+					$root_category && ModulesManager::is_module_activated('GoogleMaps')
                         ? GoogleMapsConfig::load()->get_default_marker_latitude()
                         : (
-                            $category->get_category_address() && !empty($category->get_category_address())
-                                ? TextHelper::deserialize($category_address_values)['latitude']
+							is_array($category_address_values) && isset($category_address_values['latitude'])
+								? $category_address_values['latitude']
                                 : 0
                         ),
                 'CATEGORY_LONGITUDE'   => 
-                    ModulesManager::is_module_activated('GoogleMaps') && TextHelper::is_serialized($root_category)
+					$root_category && ModulesManager::is_module_activated('GoogleMaps')
                         ? GoogleMapsConfig::load()->get_default_marker_longitude()
                         : (
-                            $category->get_category_address() && !empty($category->get_category_address())
-                            ? TextHelper::deserialize($category_address_values)['longitude']
+							is_array($category_address_values) && isset($category_address_values['longitude'])
+							? $category_address_values['longitude']
                             : 0
                         ),
 
