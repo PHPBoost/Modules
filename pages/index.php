8fdeee090dec518d2afd5c189bd7912d5847dafc
babsolune <babsolune@phpboost.com>
2026-09-20 18:20:28 +0200

    [UPD] phpdoc

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,7 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2026 06 06
+ * @version     PHPBoost 6.1 - last update: 2026 09 20
  * @since       PHPBoost 5.2 - 2020 06 15
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
@@ -13,6 +13,7 @@ define('PATH_TO_ROOT', '../..');
 
 require_once PATH_TO_ROOT . '/kernel/init.php';
 
+/** @var PagesConfig $config */
 $config  = PagesConfig::load();
 $columns = ThemesManager::get_theme(AppContext::get_current_user()->get_theme())->get_columns_disabled();
 
