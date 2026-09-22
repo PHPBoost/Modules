085e9ae964c99c71dd2c21d5328a86486e27c812
babsolune <babsolune@phpboost.com>
2026-09-22 18:03:34 +0200

    [FIX] class name

--- newsletter/controllers/NewsletterSubscribeController.class.php
+++ newsletter/controllers/NewsletterSubscribeController.class.php
@@ -3,13 +3,13 @@
  * @copyright   &copy; 2005-2026 PHPBoost
  * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
  * @author      Kevin MASSY <reidlos@phpboost.com>
- * @version     PHPBoost 6.1 - last update: 2026 05 19
+ * @version     PHPBoost 6.1 - last update: 2026 09 22
  * @since       PHPBoost 3.0 - 2011 02 08
  * @author      Julien BRISWALTER <j1.seth@phpboost.com>
  * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
 */
 
-class NewslettersubscribeController extends DefaultModuleController
+class NewsletterSubscribeController extends DefaultModuleController
 {
 	private $current_user;
 
